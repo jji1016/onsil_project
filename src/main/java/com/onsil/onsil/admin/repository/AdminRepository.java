4305b5f829106e461ac39f9cb440b8b57ad9cf8e
ekdhs3423 <ekdhs3423@naver.com>
2025-05-30 17:30:01 +0900

    admin dashboard 초안

--- src/main/java/com/onsil/onsil/admin/repository/AdminRepository.java
+++ src/main/java/com/onsil/onsil/admin/repository/AdminRepository.java
@@ -1,4 +1,11 @@
 package com.onsil.onsil.admin.repository;
 
-public class AdminRepository {
+import com.onsil.onsil.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository <Member, Long>{
+
+
 }
--- src/main/java/com/onsil/onsil/admin/service/AdminService.java
+++ src/main/java/com/onsil/onsil/admin/service/AdminService.java
@@ -1,4 +1,13 @@
 package com.onsil.onsil.admin.service;
 
+import com.onsil.onsil.admin.dao.AdminDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    private final AdminDao adminDao;
+
 }
