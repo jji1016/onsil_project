fed89ed209603806bd9a16d0381afb7337f6e0c9
ekdhs3423 <ekdhs3423@naver.com>
2025-05-30 17:30:01 +0900

    admin dashboard 초안

--- src/main/java/com/onsil/onsil/admin/controller/AdminController.java
+++ src/main/java/com/onsil/onsil/admin/controller/AdminController.java
@@ -1,4 +1,21 @@
 package com.onsil.onsil.admin.controller;
 
+import com.onsil.onsil.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/dashboard")
+    public String index() {
+
+        return "admin/dashboard";
+    }
 }
--- src/main/java/com/onsil/onsil/admin/dao/AdminDao.java
+++ src/main/java/com/onsil/onsil/admin/dao/AdminDao.java
@@ -1,4 +1,12 @@
 package com.onsil.onsil.admin.dao;
 
+import com.onsil.onsil.admin.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class AdminDao {
+
+    private final AdminRepository adminRepository;
 }
