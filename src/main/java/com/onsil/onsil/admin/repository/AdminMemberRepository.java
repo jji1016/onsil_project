48d649b1137bd562eb26e58de547fd02e1082c80
ekdhs3423 <ekdhs3423@naver.com>
2025-06-04 17:45:56 +0900

    회원 이름으로 검색기능 추가

--- src/main/java/com/onsil/onsil/admin/repository/AdminMemberRepository.java
+++ src/main/java/com/onsil/onsil/admin/repository/AdminMemberRepository.java
@@ -1,9 +1,11 @@
 package com.onsil.onsil.admin.repository;
 
+import com.onsil.onsil.admin.dto.MemberDto;
 import com.onsil.onsil.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,7 @@ public interface AdminMemberRepository extends JpaRepository <Member, Integer>{
     int deleteByUserID(String userID);
 
     Member findByUserID(String userID);
+
+    List<Member> findByUserNameContaining(String keyword);
+
 }
