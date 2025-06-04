10b215c34bf792a3e8e06390fd9fe67dd07208e7
jjang051 <acfj0895@naver.com>
2025-06-04 16:57:35 +0900

    signup 주소api & 중복검사 처음

--- src/main/java/com/onsil/onsil/member/dao/MemberDao.java
+++ src/main/java/com/onsil/onsil/member/dao/MemberDao.java
@@ -1,6 +1,7 @@
-package com.onsil.onsil.member;
+package com.onsil.onsil.member.dao;
 
 import com.onsil.onsil.entity.Member;
+import com.onsil.onsil.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +19,15 @@ public class MemberDao {
     public Optional<Member> findByUserID(String ID) {
         Optional<Member> optionalMember = memberRepository.findByUserID(ID);
         return optionalMember;
+    }
+
+    public boolean existsByUserID(String ID) {
+        return memberRepository.existsByUserID(ID);
+    }
+    public boolean existsByNickName(String nickName) {
+        return memberRepository.existsByNickName(nickName);
+    }
+    public boolean existsByUserEmail(String userEmail) {
+        return memberRepository.existsByUserEmail(userEmail);
     }
 }
