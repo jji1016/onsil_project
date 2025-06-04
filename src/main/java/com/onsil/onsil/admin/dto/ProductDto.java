eef85005f5ff3b6f91253a06409f2e75adb76530
geoche3327 <geoche3327@naver.com>
2025-06-04 18:01:27 +0900

    주문 내역, 재고 처리, 입출고 목록

---
+++ src/main/java/com/onsil/onsil/admin/dto/ProductDto.java
@@ -0,0 +1,12 @@
+package com.onsil.onsil.admin.dto;
+
+public class ProductDto {
+    private String productCode;
+    private String productName;
+    private String category;
+    private String unit;
+    private int stock;
+    private String storage;
+    private int price;
+    private boolean isActive;
+}
