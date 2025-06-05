fbdca08152ecb5794df5bf16eec782d47cf82d35
stayhumble09 <pjs980720@gmail.com>
2025-06-05 20:07:16 +0900

    Revert "Stayhumble09"

--- src/main/java/com/onsil/onsil/flower/dto/FlowerDto.java
+++ src/main/java/com/onsil/onsil/flower/dto/FlowerDto.java
@@ -16,6 +16,10 @@ public class FlowerDto {
     private String fGrow;
     private String fType;
     private String fileName1;
+    private String fileName2;
+    private String fileName3;
     private String imgUrl1;
+    private String imgUrl2;
+    private String imgUrl3;
     private String publishOrg;
 }
