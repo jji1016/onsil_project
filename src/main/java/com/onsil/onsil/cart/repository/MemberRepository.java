bffd9f4e8169f54099c500dbd27605c561af63e6
jjang051 <acfj0895@naver.com>
2025-06-05 14:17:08 +0900

    cart병합 오류수정

--- src/main/java/com/onsil/onsil/cart/repository/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.onsil.onsil.cart.repository;
-
-import com.onsil.onsil.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {}
--- src/main/java/com/onsil/onsil/member/repository/MemberRepository.java
+++ src/main/java/com/onsil/onsil/member/repository/MemberRepository.java
@@ -7,7 +7,10 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByUserID(String userID);
+
     boolean existsByUserID(String userID);          // id 중복 체크
+
     boolean existsByNickName(String nickName);  // 닉네임 중복 체크
+
     boolean existsByUserEmail(String userEmail);
 }
