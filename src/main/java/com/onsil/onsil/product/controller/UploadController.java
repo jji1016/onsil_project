bd4c4728c3843040a356d978a9b816777060a6ac
jjang051 <acfj0895@naver.com>
2025-06-10 10:17:17 +0900

    임시저장

--- src/main/java/com/onsil/onsil/product/controller/UploadController.java
+++ src/main/java/com/onsil/onsil/product/controller/UploadController.java
@@ -1,10 +1,13 @@
 package com.onsil.onsil.product.controller;
 
 import com.onsil.onsil.entity.Product;
+import com.onsil.onsil.product.dto.ProductDto;
 import com.onsil.onsil.product.repository.ProductRepository;
+import com.onsil.onsil.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/upload")
@@ -23,7 +27,8 @@ public class UploadController {
     private final ProductService productService;
     private final ProductRepository productRepository;
     @Value("${file.path}products/")
-    String productsPath;// 여기서 주입
+    String productsPath;  // 여기서 주입
+
 @GetMapping("/products")
 public String uploadForm() {
     return "upload/products";
@@ -36,10 +41,11 @@ public String saveProduct(
         @RequestParam("flowerInfo") String flowerInfo,
         @RequestParam("imageFile") MultipartFile imageFile) throws IOException {
 
-    File dir = new File(productsPath);
-    if (!dir.exists()) {
-        dir.mkdirs();
+    File saveDir = new File(productsPath);
+    if (!saveDir.exists()) {
+        saveDir.mkdirs();
     }
+
 
     String originalFilename = imageFile.getOriginalFilename(); // abc.jpg
     String extension = originalFilename.substring(originalFilename.lastIndexOf(".")); // .jpg
