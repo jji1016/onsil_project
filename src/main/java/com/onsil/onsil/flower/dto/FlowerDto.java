b51bda53a558c92b6a094090a632ab9f833240b5
stayhumble09 <pjs980720@gmail.com>
2025-06-12 18:13:36 +0900

    api db에 넣는중 트래픽문제로 인해 오류, 수정 예정

--- src/main/java/com/onsil/onsil/flower/dto/FlowerDto.java
+++ src/main/java/com/onsil/onsil/flower/dto/FlowerDto.java
@@ -1,38 +1,34 @@
 package com.onsil.onsil.flower.dto;
 
-import com.onsil.onsil.entity.Flower;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.onsil.onsil.entity.Product;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class FlowerDto {
-    @JsonProperty("dataNo")
+    private Integer id;
     private Integer dataNo;
-    @JsonProperty("fMonth")
     private Integer fMonth;
-    @JsonProperty("flowNm")
-    private String flowNm;
-    @JsonProperty("flowLang")
+    private String flowerName;
     private String flowLang;
-    @JsonProperty("fContent")
-    private String fContent;
-    @JsonProperty("fUse")
+    private String flowerInfo;
     private String fUse;
-    @JsonProperty("fGrow")
     private String fGrow;
-    @JsonProperty("fType")
     private String fType;
-    @JsonProperty("publishOrg")
+    private int price;
+    private String image;
     private final String publishOrg = "농촌진흥청 국립원예특작과학원";
 
-    public FlowerDto(Flower flower) {
-        this.dataNo = flower.getDataNo();
-        this.fMonth = flower.getFMonth();
-        this.flowNm = flower.getFlowNm();
-        this.flowLang = flower.getFlowLang();
-        this.fContent = flower.getFContent();
-        this.fUse = flower.getFUse();
-        this.fGrow = flower.getFGrow();
-        this.fType = flower.getFType();
+    public FlowerDto(Product product) {
+        this.id = product.getId();
+        this.dataNo = product.getDataNo();
+        this.fMonth = product.getFMonth();
+        this.flowerName = product.getFlowerName();
+        this.flowLang = product.getFlowLang();
+        this.flowerInfo = product.getFlowerInfo();
+        this.fUse = product.getFUse();
+        this.fGrow = product.getFGrow();
+        this.fType = product.getFType();
+        this.price = product.getPrice();
+        this.image = product.getImage();
     }
 }
